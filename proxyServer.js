48bde011eb705c634894438c7c43564f00102cee
Kunal Bhatia <bhatiakbkb@gmail.com>
2024-06-21 13:36:10 +0530

    port change

--- proxyServer.js
+++ proxyServer.js
@@ -21,7 +21,7 @@ app.get('/news', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Proxy server listening on port ${PORT}`);
 });
